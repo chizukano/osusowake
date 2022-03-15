e6ec159b125c0eda23fec143ebe49ff6f6d4efec
Brianna Foley <brianna.foley87@gmail.com>
2022-03-15 12:46:00 -0600

    created seeds for 5 models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,61 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "---creating fake users---"
+default_password = "123456"
+user1 = User.create!(
+  email: "user1@users.com",
+  password: default_password
+)
+user2 = User.create!(
+  email: "user2@users.com",
+  password: default_password
+)
+user3 = User.create!(
+  email: "user3@users.com",
+  password: default_password
+)
+puts "---done---"
+users = [user1, user2, user3]
+
+puts "---creating fake chefs---"
+chef1 = Chef.create!(
+  address: "Amsterdam 300a, Hipódromo, Cuauhtémoc, 06100 Ciudad de México, CDMX",
+  user: user1
+)
+chef2 = Chef.create!(
+  address: "Guanajuato 165, Roma Nte., Cuauhtémoc, 06700 Ciudad de México, CDMX",
+  user: user2
+)
+puts "---done---"
+chefs = [chef1, chef2]
+
+meals = []
+puts "---creating fake meals---"
+10.times do
+  meals << Meal.create!(
+    name: Faker::Food.dish,
+    description: Faker::Food.description,
+    chef: chefs.sample
+  )
+end
+puts "---done---"
+
+cooking_sessions = []
+puts "---creating fake cooking_sessions---"
+10.times do
+  cooking_sessions << CookingSession.create!(
+    max_portions: rand(2..10),
+    start_at: Faker::Time.forward(days: 2, period: :morning, format: :long),
+    end_at: Faker::Time.forward(days: 2, period: :afternoon, format: :long),
+    meal: meals.sample
+  )
+end
+puts "---done---"
+
+puts "---creating fake reservations---"
+5.times do
+  Reservation.create!(
+    portion_count: rand(2..10),
+    user: users.sample,
+    cooking_session: cooking_sessions.sample
+  )
+end
+puts "---done---"
