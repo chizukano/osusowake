06022d1bb261ff95c88d3b2d27c01ebc0181468c
chizukano <chizu0303@gmail.com>
2022-03-21 17:04:03 -0600

    Add datetime config and package

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -3,6 +3,9 @@ import flatpickr from "flatpickr"
 
 export default class extends Controller {
   connect() {
-    flatpickr(this.element);
+    flatpickr(this.element, {
+      enableTime: true,
+      dateFormat: "Y-m-d H:i",
+    });
   }
 }
