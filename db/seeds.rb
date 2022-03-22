bc979436a7f2526d8620be23933b248dc7f76dd0
chizukano <chizu0303@gmail.com>
2022-03-22 15:25:33 -0600

    seeds for more precise addresses & description

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,31 +51,31 @@ puts "---done---"
 
 puts "---creating fake chefs---"
 chef1 = Chef.create!(
-  address: "Amsterdam 300a, CDMX",
+  address: "Amsterdam 300, Hipódromo, Mexico City, CDMX",
   user: user1
 )
 chef2 = Chef.create!(
-  address: "Tlacotalpan 30, CDMX",
+  address: "Tlacotalpan 30, Roma Sur, Mexico City, CDMX",
   user: user2
 )
 chef3 = Chef.create!(
-  address: "Av Mexico 49, CDMX",
+  address: "Av México 49, Hipódromo, Mexico City, CDMX",
   user: user4
 )
 chef4 = Chef.create!(
-  address: "Tapachula 88, CDMX",
+  address: "Tapachula 88, Roma Norte, Mexico City, CDMX",
   user: user5
 )
 chef5 = Chef.create!(
-  address: "Calle de Durango 289, CDMX",
+  address: "Avenida Tamaulipas 30, La Condesa, Mexico City, CDMX",
   user: user6
 )
 chef6 = Chef.create!(
-  address: "Puebla 77, CDMX",
+  address: "Puebla 77, Roma Norte, Mexico City, CDMX",
   user: user7
 )
 chef7 = Chef.create!(
-  address: "Culiacan 26, CDMX",
+  address: "Culiacan 26, Hipódromo, Mexico City, CDMX",
   user: user8
 )
 puts "---done---"
@@ -85,9 +85,9 @@ puts "---creating fake meals---"
 
 meal_names = ["Chicken Butter Curry", "Caesar Salad", "Chicken Ramen", "Green Salad",
               "Guacamole Dip", "Shrimp Pasta with Creamy Tomato Sauce",
-              "Fresh Pasta", "Salmon with Tomato Soup", "Chocolate Chip Cookies",
-              "Meatball Pasta", "Wonton Soup", "Cinnamon Roll", "Walnut Bread", "Pasta Salad",
-              "Arugula Mozzarella Pizza", "Chocolate Donuts", "Cauliflower Dip"]
+              "Fresh Pasta", "Salmon with Tomato Soup", "Chocolate Chip Cookies", "Wonton Soup",
+              "Meatball Pasta", "Cinnamon Roll", "Walnut Bread", "Pasta Salad", "Steamed Dumplings",
+              "Arugula Mozzarella Pizza", "Chocolate Donuts", "Cauliflower Dip", "Taiyaki Waffle"]
 
 meals = []
 meal_names.map do |meal_name|
@@ -101,14 +101,23 @@ meal_names.map do |meal_name|
   meal.save!
   meals << meal
 end
+
+meals.first.update(description: "Finger-Lickin’ chicken butter curry (Murgh Makhani) - chicken with a spiced tomato and butter (makhan) sauce.
+  This special dish from my grandma's secret recipe makes the butter sauce creamy yet not too heavy.
+  It goes well with the tender and juicy chicken thanks to a deliciously spiced yogurt marinade. My neighbours LOVE this dish. It is now your turn to try!")
+meals.last.update(description: "Taiyaki is a Japanese fish-shaped waffle, commonly sold as street food. It imitates the shape of tai (red sea bream), which it is named after.
+  It is crispy on the outside, fluffy on the inside and filled with sweet red bean paste. It is very rare to find this Japanese waffle in Mexico.
+  It often sells out quickly, so take your opportunity and try Taiyaki at home!")
+puts "done!"
+
 puts "---done---"
 
 puts "---creating fake past cooking sessions---"
 cooking_sessions = []
-meals[0..2].each do |meal|
+meals[0..3].each do |meal|
   chef = meal.chef
   cooking_sessions << CookingSession.create!(
-    max_portions: rand(2..10),
+    max_portions: rand(2..6),
     address: chef.address,
     longitude: chef.longitude,
     latitude: chef.latitude,
@@ -118,10 +127,10 @@ meals[0..2].each do |meal|
   )
 end
 
-meals[3..5].each do |meal|
+meals[4..6].each do |meal|
   chef = meal.chef
   cooking_sessions << CookingSession.create!(
-    max_portions: rand(2..10),
+    max_portions: rand(2..6),
     address: chef.address,
     longitude: chef.longitude,
     latitude: chef.latitude,
@@ -133,25 +142,25 @@ end
 puts "---done---"
 
 puts "---creating fake near future cooking sessions---"
-meals[6..13].each do |meal|
+meals[7..13].each do |meal|
   chef = meal.chef
   cooking_sessions << CookingSession.create!(
-    max_portions: rand(2..10),
+    max_portions: rand(2..6),
     address: chef.address,
     longitude: chef.longitude,
     latitude: chef.latitude,
-    start_at: Time.current + rand(2..3).hours,
-    end_at: Time.current + rand(4..5).hours,
+    start_at: Time.current + rand(1..2).hours,
+    end_at: Time.current + rand(3..5).hours,
     meal: meal
   )
 end
 puts "---done---"
 
 puts "---creating fake future cooking sessions---"
-meals[14..16].each do |meal|
+meals[14..19].each do |meal|
   chef = meal.chef
   cooking_sessions << CookingSession.create!(
-    max_portions: rand(2..10),
+    max_portions: rand(2..6),
     address: chef.address,
     longitude: chef.longitude,
     latitude: chef.latitude,
@@ -160,19 +169,7 @@ meals[14..16].each do |meal|
     meal: meal
   )
 end
-# 3.times do
-#   meal = meals.sample
-#   chef = meal.chef
-#   cooking_sessions << CookingSession.create!(
-#     max_portions: rand(6..10),
-#     address: chef.address,
-#     longitude: chef.longitude,
-#     latitude: chef.latitude,
-#     start_at: Faker::Time.forward(days: 1, period: :afternoon, format: :long),
-#     end_at: Faker::Time.forward(days: 1, period: :evening, format: :long),
-#     meal: meal
-#   )
-# end
+
 puts "---done---"
 
 puts "---creating fake reservations---"
